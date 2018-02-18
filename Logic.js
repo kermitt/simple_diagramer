0b4c7d00918b6cf3d085fdd0fb2a74ace1b8bf8a
Montgomery <r621362@TRG519305.domain>
2018-02-17 17:58:19 -0800

    some cleanup + getting ready to draw lines

--- Logic.js
+++ Logic.js
@@ -1,24 +1,24 @@
 
+let DIAGRAM = d3.select('#board')
+
 let node_drag = d3.behavior.drag()
     .on('drag', function (d, i) {
       if (STATE === NORMAL_MODE) {
-        console.log('State: ' + STATE)
         d.x += d3.event.dx
         d.y += d3.event.dy
         d3.select(this).attr('transform', function (d, i) { return 'translate(' + [d.x, d.y] + ')' })
       }
     })
-    .on('dragstart', function (d, i) {
-    })
-    .on('dragend', function (d, i) {
-      console.log('Let go of ' + this.id)
-    })
-d3.select('#board').on('click', function () {
-  let coords = d3.mouse(this)
+    .on('dragstart', function (d, i) {})
+    .on('dragend', function (d, i) {})
 
+// d3.select('#board').on('click', function () {
+DIAGRAM.on('click', function () {
+  let coords = d3.mouse(this)
   if (STATE === NORMAL_MODE) {
-//    console.log(this)
   } else if (STATE === NODE_MODE) {
     addNode(coords[0], coords[1])
+  } else if (STATE === EDGE_MODE) {
+
   }
 })
