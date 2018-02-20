1e0ff1af1d2b2bcdfa8dfbf97d8b1be159560b15
Montgomery <r621362@TRG519305.domain>
2018-02-19 17:28:04 -0800

    TODO: Add paths to nodes

--- Logic.js
+++ Logic.js
@@ -52,10 +52,12 @@ const dragstarted = () => {
       NODES[this.id].x = x1
       NODES[this.id].y = y1
     } else if (STATE === EDGE_MODE) {
+      // show the destination
+      metro_line.classed('metro_line', true)
+      metro_circle.classed('metro_cirle', true)
+
       //  draw an edge
-
       fromTo.setFrom(this.id)
-
       if (dx * dx + dy * dy > 100) d.push([x0 = x1, y0 = y1])
       else d[d.length - 1] = [x1, y1]
       line.id = id
@@ -77,9 +79,31 @@ const dragstarted = () => {
 }
 const dragended = () => {
   if (fromTo !== undefined) {
-    console.log('|' + JSON.stringify(fromTo, null, 6) + '|')
+    drawEdge(fromTo)
+    DIAGRAM.select('#' + fromTo.id).remove()
     fromTo = undefined
+    metro_line.classed('metro_line', false)
+    metro_circle.classed('metro_cirle', false)
   }
+}
+
+const drawEdge = (fromTo) => {
+  console.log('|' + JSON.stringify(fromTo, null, 6) + '|')
+
+  let from = NODES[fromTo.from]
+  let to = NODES[fromTo.to]
+
+  let middleX = (from.x + to.x) / 2
+  let middleY = (from.y + to.y) / 2
+
+  const context = d3.path()
+  context.moveTo(from.x, from.y)
+  context.lineTo(middleX, middleY)
+  context.lineTo(to.x, to.y)
+
+  DIAGRAM.append('path')
+    .attr('class', 'path')
+    .attr('d', context.toString())
 }
 
 const euclide = (point) => {
